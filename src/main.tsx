e25561e4a6063bfef03ec564a6c8bcacabd97f0e
Eduardo Anaya <gran_ed@hotmail.com>
2025-03-18 08:10:20 +0100

    fix visibility issues

--- src/main.tsx
+++ src/main.tsx
@@ -17,7 +17,8 @@ const darkTheme = createTheme({
   }
 })
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+const root = ReactDOM.createRoot(document.getElementById('root')!)
+root.render(
   <React.StrictMode>
     <ThemeProvider theme={darkTheme}>
       <CssBaseline />
